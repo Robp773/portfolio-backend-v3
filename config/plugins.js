d4701fd79d4205ba6b82440fc96b0682341de151
Robert Peterman <robpeterman573@gmail.com>
2022-04-24 12:43:06 -0400

    Initial commit - starting with gatsby v3 to get around v4 cloudinary issues

---
+++ config/plugins.js
@@ -0,0 +1,19 @@
+if (process.env.NODE_ENV === "production") {
+  module.exports = ({ env }) => ({
+    upload: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+    // ...
+  });
+} else {
+  module.exports = {};
+}
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '712243d5-79d0-43cb-bfd9-af80bf4c9378'
+};
